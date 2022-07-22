608d711ff9935f5f8c5b7cc2a70434f378ea190b
li-li-github <liamiiliil@gmail.com>
2022-07-22 16:07:50 -0700

    added data, requirements.txt; updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # napari_3D_rotation
 Generate rotating 3D UMAP in napari
+
+## Installation
+```bash
+conda create -n napari3drot python=3.9
+conda activate napari3drot
+pip install -r requirements.txt
+```
+
+
+## Sample code
+```python
+from os.path import join
+import napari
+import numpy as np
+import seaborn as sns
+from napari_animation import Animation
+
+# Load data
+data_name = 'cuml_umap3D_vqindhist2_nb1000_md0'
+umap3d_data = np.load(join('data', data_name + '.npy'))
+umap_lab = np.load(join('data', 'test_label_nucenter_uniorg_corum.npy'), allow_pickle=True)
+
+# Make a color matrix for uniorg
+uniq_uniorg = np.unique(umap_lab[:, 0])
+lab_color = np.zeros((len(umap_lab), 4))
+cmap = sns.color_palette("hls", len(uniq_uniorg))
+for i, fmly in enumerate(uniq_uniorg):
+    if fmly == 'others':
+        c = np.array(sns.color_palette('Greys', 100)[9] + (0.25,)).reshape(1, -1)
+    else:
+        c = np.array(cmap[i] + (1,)).reshape(1, -1)
+    ind = umap_lab[:, 0] == fmly
+    lab_color[ind] = c
+
+viewer = napari.view_points(
+    umap3d_data, scale=(100,) * 3, shading='spherical', size=0.06, name='umap3d_nb1000', edge_width=0,
+    face_color=lab_color, ndisplay=3,
+)
+viewer.window.resize(1300, 1000)
+
+# Set parameters for the video
+scale_factor = 0.5  # scale factor for the final output video size
+nb_steps = 30  # number of steps between two target angles
+# Start shooting video
+animation = Animation(viewer)
+viewer.dims.ndisplay = 3
+viewer.reset_view()
+original_zoom = viewer.camera.zoom
+viewer.camera.angles = (0.0, 0.0, 90.0)
+animation.capture_keyframe()
+viewer.camera.angles = (0.0, 180.0, 90.0)
+animation.capture_keyframe(steps=nb_steps)
+viewer.camera.angles = (0.0, 360.0, 90.0)
+animation.capture_keyframe(steps=nb_steps)
+animation.animate(f'demo.gif', canvas_only=True, fps=20, scale_factor=scale_factor)
+
+```
+
+![Rotating GIF](demo.gif)
+
+Supported output formats are
+`.gif`, `.mp4`, `.mov`, `.avi`, `.mpg`, `.mpeg`, `.mkv`, `.wmv`
+If no extension is provided, images are saved as a folder of PNGs
--- test_run.py
+++ test_run.py
@@ -4,50 +4,53 @@ This script creates a rotating 3D UMAP in napari viewer and record it as a video
 from os.path import join
 import napari
 import numpy as np
-from matplotlib import cm
+import seaborn as sns
+from napari_animation import Animation
 
 # Load data
-data_name = 'cuml_umap3D_vqindhist2_nb1000_md0.npy'
-umap3d_data = np.load(join(
-    '/mnt/ESS/opencell/results/fulldata/VQVAE2D/selfsupervised/efb0_chspl_dhstfc_gcont_splitFOV/'
-    'z11[25, 25, 64]2048_z29[4, 4, 64]2048/cmmtcost_0.25_0.25/fc2_1000_vq[2]_do[0.5]/lw_d1_f1_v1/'
-    '1311_[100, 100]_[gfp, nuc]_nucenter/st0_pr2_sp1/lr0.001/rep1/embeddings', data_name)
-)
-umap_lab = np.load(
-    '/mnt/ESS/opencell/results/fulldata/VQVAE2D/selfsupervised/efb0_chspl_dhstfc_gcont_splitFOV/'
-    'z11[25, 25, 64]2048_z29[4, 4, 64]2048/cmmtcost_0.25_0.25/fc2_1000_vq[2]_do[0.5]/lw_d1_f1_v1/'
-    '1311_[100, 100]_[gfp, nuc]_nucenter/st0_pr2_sp1/lr0.001/rep1/embeddings/test_label_nucenter_uniorg_corum.npy',
-    allow_pickle=True
-)
+data_name = 'cuml_umap3D_vqindhist2_nb1000_md0'
+umap3d_data = np.load(join('data', data_name + '.npy'))
+umap_lab = np.load(join('data', 'test_label_nucenter_uniorg_corum.npy'), allow_pickle=True)
 
 # Make a color matrix for uniorg
 uniq_uniorg = np.unique(umap_lab[:, 0])
 lab_color = np.zeros((len(umap_lab), 4))
-cmap = cm.get_cmap('tab20').colors
+cmap = sns.color_palette("hls", len(uniq_uniorg))
 for i, fmly in enumerate(uniq_uniorg):
     if fmly == 'others':
-        c = np.array(cm.Greys(25)[:-1] + (0.1,)).reshape(1, -1)
+        c = np.array(sns.color_palette('Greys', 100)[9] + (0.25,)).reshape(1, -1)
     else:
         c = np.array(cmap[i] + (1,)).reshape(1, -1)
     ind = umap_lab[:, 0] == fmly
     lab_color[ind] = c
 
 viewer = napari.view_points(
-    umap3d_data, scale=(100,) * 3, shading='spherical', size=0.03, name='umap3d_nb1000', edge_width=0,
+    umap3d_data, scale=(100,) * 3, shading='spherical', size=0.06, name='umap3d_nb1000', edge_width=0,
     face_color=lab_color, ndisplay=3,
 )
+viewer.window.resize(1300, 1000)
 
 
 # Record animation
-from napari_animation import Animation
-
 animation = Animation(viewer)
 viewer.dims.ndisplay = 3
+nb_steps = 3*60
 viewer.reset_view()
+original_zoom = viewer.camera.zoom
 viewer.camera.angles = (0.0, 0.0, 90.0)
 animation.capture_keyframe()
 viewer.camera.angles = (0.0, 180.0, 90.0)
-animation.capture_keyframe(steps=60)
+animation.capture_keyframe(steps=nb_steps)
 viewer.camera.angles = (0.0, 360.0, 90.0)
-animation.capture_keyframe(steps=60)
-animation.animate(f'{data_name[:-4]}.mov', canvas_only=True)
+animation.capture_keyframe(steps=nb_steps)
+viewer.camera.zoom = 1.3
+viewer.camera.angles = (0.0, 180.0+360, 90.0)
+animation.capture_keyframe(steps=nb_steps)
+viewer.camera.angles = (0.0, 360.0+360, 90.0)
+animation.capture_keyframe(steps=nb_steps)
+viewer.camera.zoom = original_zoom
+viewer.camera.angles = (0.0, 360.0+360+180, 90.0)
+animation.capture_keyframe(steps=nb_steps)
+viewer.camera.angles = (0.0, 360.0+360+360, 90.0)
+animation.capture_keyframe(steps=nb_steps)
+animation.animate(f'{data_name[:-4]}2.mov', canvas_only=True, fps=60)
